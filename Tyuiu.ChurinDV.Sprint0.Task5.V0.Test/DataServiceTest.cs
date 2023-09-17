daced4e2f72a656f542fefc4db44fa2489e46a87
ChurinDmitriy <mister.dim2017111@gmail.com>
2023-09-17 19:12:15 +0500

    Task5

---
+++ Tyuiu.ChurinDV.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ChurinDV.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.ChurinDV.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(10, DataService.Multiplication(10, 5));
+        }
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(10, DataService.Division(9, 3));
+        }
+    }
+}
